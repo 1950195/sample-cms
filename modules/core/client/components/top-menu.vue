220e270faf9f57e71c115143afe755ed98ad87ea
Lionel Dong <1950195@qq.com>
2016-01-12 16:57:19 +0800

    added top-menu vue

---
+++ modules/core/client/components/top-menu.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="nav navbar-nav">
+        <li>
+            <a href="#" class="menu-item">Link</a>
+        </li>
+        <li class="dropdown">
+            <a href="#" class="menu-item dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <li><a role="menuitem" class="sub menu-item" href="#">Action</a></li>
+                <li><a role="menuitem" class="sub menu-item" href="#">Another action</a></li>
+                <li><a role="menuitem" class="sub menu-item" href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a role="menuitem" class="sub menu-item" href="#">Separated link</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a role="menuitem" class="sub menu-item" href="#">One more separated link</a></li>
+            </ul>
+        </li>
+    </ul>
+</template>
--- modules/core/client/components/top.vue
+++ modules/core/client/components/top.vue
@@ -5,23 +5,7 @@
                 <a href="#" class="navbar-brand">{{title}}</a>
             </div>
             <div class="navbar-container" aria-expanded="false">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#" class="menu-item">Link</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="menu-item dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a role="menuitem" class="sub menu-item" href="#">Action</a></li>
-                            <li><a role="menuitem" class="sub menu-item" href="#">Another action</a></li>
-                            <li><a role="menuitem" class="sub menu-item" href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a role="menuitem" class="sub menu-item" href="#">Separated link</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a role="menuitem" class="sub menu-item" href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <top-menu></top-menu>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#" class="menu-item">Sign In</a></li>
                     <li><a href="#" class="menu-item">Sign Up</a></li>
@@ -32,11 +16,16 @@
 </template>
 
 <script>
+    import TopMenu from './top-menu.vue';
+
     export default {
         data () {
             return {
                 title: 'CMS'
             }
+        },
+        components: {
+            TopMenu
         }
     }
 </script>
